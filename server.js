8c1bd4987a2b83f433a4d1e023368e390f7439c5
troylochner <troy.lochner@gmail.com>
2021-01-09 15:05:18 -0500

    added routes + monoggose

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+const routes = require("./routes")
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
