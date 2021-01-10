1236da013c714990a9663fb608361b3e941686a6
troylochner <troy.lochner@gmail.com>
2021-01-09 19:00:58 -0500

    adding in dom router ; background page components

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+//use react browser routher
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+//keep hashrouter handy - as I had a previous issue with this :
+//import { HashRouter as Router, Switch, Route, BrowserRouter } from 'react-router-dom'
+
+import Search from "./pages/Search"
+import SavedBook from "./pages/SavedBook"  
+
+
 //IMPORT ALL COMPONENTS FOR NOW (NOT SUPER EFFICIENT BUT WHATEVER)
 import Header from "./components/header";
 import Footer from "./components/footer";
@@ -10,13 +19,19 @@ import Results from "./components/results";
 import BookInfo from "./components/book-info";
 import SearchBar from "./components/searchbar";
 
+
+
 class App extends Component {
   render() {
     return (
+    
+    <Router>
       <div className="App">
         <div className="App-header">
+          
           <Navigation/>
           <Header/>
+          
 
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -31,7 +46,9 @@ class App extends Component {
 
 
         <Footer/>
+
       </div>
+      </Router>
    
     );
   }
